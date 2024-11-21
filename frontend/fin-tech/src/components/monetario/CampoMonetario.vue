f8f728b9cacf2887ddea75b33364c85267004d60
patrickMachado12 <ti_ko_silva@hotmail.com>
2024-11-21 17:34:47 -0300

    🐛 Ajuste componente de campo monetário.

--- frontend/fin-tech/src/components/monetario/CampoMonetario.vue
+++ frontend/fin-tech/src/components/monetario/CampoMonetario.vue
@@ -4,7 +4,7 @@
       :id="'textfield-' + id"
       :label="label"
       v-money="formatoMonetario"
-      v-model.lazy="valor"
+      v-model="valor"
       @change="emitirAlteracoes"
       clearable
     />
@@ -20,37 +20,53 @@ export default {
   props: {
     id: { type: String, required: false, default: () => 'monetario' },
     label: { type: String, required: true },
-    entrada: { type: Number, default: 0 },
+    entrada: { type: [Number, String], default: 0 },
   },
   data() {
     return {
-      valor: 0,
+      valor: this.formatarValorMonetario(this.entrada),
       formatoMonetario: {
         decimal: ',',
         thousands: '.',
         prefix: 'R$ ',
         precision: 2,
-        masked: false
+        masked: true
       }
     };
   },
   methods: {
+    // Emitir a alteração para o componente pai, removendo a máscara do valor
     emitirAlteracoes() {
-      this.$emit("change", parseFloat(this.removerMascara(this.valor)));
+      this.$emit("change", this.removerMascara(this.valor));
     },
 
+    // Remover a máscara do valor, caso o valor seja uma string formatada
     removerMascara(value) {
-      if (typeof value == "string") {
+      if (typeof value === "string") {
         return parseFloat(
           value
             .replace("R$ ", "")
             .replace(/\./g, "")
             .replace(",", ".")
         );
-      } else if (typeof value == "number") {
+      } else if (typeof value === "number") {
         return value;
       }
       return 0;
+    },
+
+    // Formatando o valor inicial para o formato monetário
+    formatarValorMonetario(valor) {
+      if (typeof valor === "number") {
+        return valor.toFixed(2).replace('.', ',');
+      }
+      return valor;
+    }
+  },
+  watch: {
+    // Atualizar o valor caso o prop "entrada" seja alterado
+    entrada(newValor) {
+      this.valor = this.formatarValorMonetario(newValor);
     }
   }
 };
